22a7365de62e0b6203942106e57d294d5faef596
Evandro <110628201+EvandroCalado@users.noreply.github.com>
2022-11-29 01:53:03 -0300

    update

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,9 +1,11 @@
+import "./styles.css"
+
 export const PostCard = ({ title, body, id, cover }) => {
   return (
     <div className="post">
       <img src={cover} alt={title} />
       <div className="post-content">
-        <h1>{title}</h1>
+        <h2>{title} {id}</h2>
         <p>{body}</p>
       </div>
     </div>
